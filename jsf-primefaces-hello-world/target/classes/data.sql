e54febb8860fb3386ae0079b66eda6b348e1f65e
Admin <Admin@192.168.124.148>
2019-03-28 08:55:32 +0100

    Funciones de Buscar, Crear, Editar y Borrar (CRUD) funciona correctament

--- jsf-primefaces-hello-world/target/classes/data.sql
+++ jsf-primefaces-hello-world/target/classes/data.sql
@@ -8,3 +8,33 @@ insert into pelicula (nombre, anio , premios) values ('Nosotros', '2019', '1');
 
 insert into pelicula (nombre, anio, premios) values ('El Señor De Los Anillos: Las Dos Torres', '2012', '5');
 
+insert into pelicula (nombre, anio, premios) values ('Mad Max: Furia En La Carretera', '2016', '1');
+
+insert into pelicula (nombre, anio, premios) values ('El Lobo De Wall Street', '2017', '1');
+
+insert into pelicula (nombre, anio, premios) values ('El Hobbit', '2012', '0');
+
+insert into pelicula (nombre, anio, premios) values ('Batman Begins', '2012', '2');
+
+insert into pelicula (nombre, anio, premios) values ('Creep', '2012', '0');
+
+insert into pelicula (nombre, anio, premios) values ('El Gran Hotel Budapest', '2017', '1');
+
+insert into pelicula (nombre, anio, premios) values ('Isla De Perros', '2017', '5');
+
+insert into pelicula (nombre, anio, premios) values ('Ave Cesar!', '2016', '1');
+
+insert into pelicula (nombre, anio, premios) values ('Pequeños Guerreros', '1998', '1');
+
+insert into pelicula (nombre, anio, premios) values ('Pulp Fiction', '1992', '1');
+
+insert into pelicula (nombre, anio, premios) values ('Aladin', '2019', '0');
+
+insert into pelicula (nombre, anio, premios) values ('Pesadillas Antes De Navidad', '1994', '1');
+
+insert into pelicula (nombre, anio, premios) values ('Los Odiosos Ocho', '2017', '1');
+
+insert into pelicula (nombre, anio, premios) values ('Starship Troopers', '1995', '1');
+
+insert into pelicula (nombre, anio, premios) values ('Infiltrados', '2012', '1');
+
