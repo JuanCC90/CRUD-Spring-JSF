69fcee2555d262968054e1ea88da3f3566bda29b
Admin <Admin@192.168.124.148>
2019-03-29 09:26:27 +0100

    Me peta al haber cambiado el modo de busqueda

--- jsf-primefaces-hello-world/src/main/java/com/codenotfound/primefaces/PeliculaRepository.java
+++ jsf-primefaces-hello-world/src/main/java/com/codenotfound/primefaces/PeliculaRepository.java
@@ -12,20 +12,15 @@ public interface PeliculaRepository extends CrudRepository<Pelicula, Long> {
 	@Query("select p from Pelicula where p.id like (:id) and p.nombre like (:nombre) and p.anio like (:anio) and p.premios like (:premios) ")
 	List<Pelicula> findByNombre(@Param("id") long id, @Param("nombre") String nombre, @Param("anio") String anio, @Param("premios") long premios);
 	
-	@Query("select p from Pelicula where p.anio like(:anio)")
-	List<Pelicula> findByAnio(@Param("anio")String anio);
 	
 	Pelicula getById(long id);
 	
 	List<Pelicula> findAll();
 	
-	@Query("select p from Pelicula where p.id like (:id)")
-	Pelicula findById(@Param ("id")long id);
+	
+	Pelicula findById(long id);
 	
 	
 	Pelicula deleteById(long id);
 	
-	@Query("select p from Pelicula where p.premios like (:premios)")
-	PeliculaDTO findByPremios(@Param ("premios")long premios);
-
 }
